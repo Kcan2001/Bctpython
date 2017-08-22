50045f43685a5bdc73d5773957d9a76614da4580
Igor Dubovik <idubovik@milosolutions.com>
2017-08-22 13:50:24 +0300

    Url changes in different apps

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,17 +1,34 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import logout
-from .views import UserLoginView, LoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
+    # Account home page
     url(r'^$', views.myaccount, name='myaccount'),
-    url(r'^login/$', UserLoginView.as_view(), name='login'),
-    url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
-    # url(r'^logout/', views.logoutview,  name='logout'),
+
+    # Sign up page for user
     url(r'^signup/', views.signup, name='signup'),
-    url(r'^update-account/(?P<user_id>[\w-]+)/$', views.AccountUpdate.as_view(), name='update-account'),
-    url(r'^update-user/(?P<pk>[\w-]+)/$', views.UserUpdate.as_view(), name='update-user')
+
+    # Login and Logout pages for account
+    url(r'^login/$', views.UserLoginView.as_view(), name='login'),
+    url(r'^logout/$', views.UserLogoutView.as_view(), name='logout'),
+
+    # Update pages for User and Account models
+    url(r'^update-account/$', views.AccountUpdate.as_view(), name='update_account'),
+    url(r'^update-user/$', views.UserUpdate.as_view(), name='update_user'),
+
+    # URLs for password change. password_change - page with form to input old password.
+    url(r'^password-change/$', views.UserPasswordChangeView.as_view(), name='password_change'),
+    url(r'^password-change-done/$', views.UserPasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # URLs for password reset ('forgot password' functionality). _reset and _confirm - pages with forms
+    url(r'^password-reset/$', views.UserPasswordResetView.as_view(), name='password_reset'),
+    url(r'^password-reset-done/$', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^password-reset-confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.UserPasswordResetConfirm.as_view(), name='password_reset_confirm'),
+    url(r'^password-reset-complete/$', views.UserPasswordResetComplete.as_view(), name='password_reset_complete'),
+
+    # One-row logout (for backup purposes)
+    # url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
 ]
