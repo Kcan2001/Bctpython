793e9a277c96a77d6651b33a5b6d9c6d3109fda7
Igor Dubovik <idubovik@milosolutions.com>
2017-08-22 20:27:13 +0300

    Add user signup with email activation

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Some fields will be null=True, while I will create normal signup/update view
@@ -14,6 +16,14 @@ class Account(models.Model):
     passport_nationality = models.CharField(max_length=30, blank=True)
     passport_issue_date = models.DateField(null=True, blank=True)
     passport_expire_date = models.DateField(null=True, blank=True)
+    email_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def update_user_account(sender, instance, created, **kwargs):
+    if created:
+        Account.objects.create(user=instance)
+    instance.account.save()
