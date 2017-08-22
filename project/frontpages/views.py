e5923a338f8f57d98b974b42fc05789f6351f29f
Igor Dubovik <idubovik@milosolutions.com>
2017-08-22 13:51:08 +0300

    Add CBV for login, logout, password change, reset, update, etc.

--- project/frontpages/views.py
+++ project/frontpages/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, redirect, render_to_response
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
 from django.template import RequestContext
 
 
@@ -16,48 +14,12 @@ def gallery(request):
     return render(request, 'frontpages/gallery.html')
 
 
-def loginview(request):
-    if request.method == 'POST':
-        user = authenticate(username=request.POST['username'], password=request.POST['password'])
-        if user is not None:
-            login(request, user)
-            if 'next' in request.POST:
-                return redirect(request.POST['next'])
-            return redirect('accounts:myaccount')
-        else:
-            return render(request, 'frontpages/login.html', {'error': 'The Username and Password did not match.'})
-    else:
-        return render(request, 'frontpages/login.html')
-
-
-def logoutview(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('home')
-
-
-def signup(request):
-    if request.method == 'POST':
-        if request.POST['password'] == request.POST['password2']:
-            try:
-                user = User.objects.get(username=request.POST['username'])
-                return render(request, 'frontpages/signup.html', {'error': 'Username already in use, Please try another.'})
-            except User.DoesNotExist:
-                user = User.objects.create_user(first_name=request.POST['firstname'], last_name=request.POST['lastname'], email=request.POST['email'], username=request.POST['username'], password=request.POST['password'])
-                login(request, user)
-                return render(request, 'accounts/myaccount.html')
-        else:
-            return render(request, 'frontpages/signup.html', {'error': 'Passwords did not match.'})
-    else:
-        return render(request, 'frontpages/signup.html')
-
-
 def yourtrips(request):
     return render(request, 'frontpages/yourtrips.html')
 
 
-def accom(request):
-    return render(request, 'frontpages/accom.html')
+def accommodations(request):
+    return render(request, 'frontpages/accommodations.html')
 
 
 def guides(request):
