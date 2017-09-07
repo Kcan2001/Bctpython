c93320c9da5daff89b846138b6670e71765f92bc
Igor Dubovik <idubovik@milosolutions.com>
2017-09-07 12:13:54 +0300

    Register models in admin panel

--- project/trips/admin.py
+++ project/trips/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
 
-from .models import Trip, TripImage, Excursion
+from .models import Trip, TripDate, TripImage, Excursion
 
 
-class TripAdmin(admin.ModelAdmin):
-    date_hierarchy = 'arrival'
-    search_fields = ['title', 'description']
-    list_display = ['title', 'price', 'arrival', 'departure']
-    list_filter = ['price', 'arrival']
-    prepopulated_field = {'slug': ('title',)}
+# class TripAdmin(admin.ModelAdmin):
+#     date_hierarchy = 'arrival'
+#     search_fields = ['title', 'description']
+#     list_display = ['title', 'price', 'arrival', 'departure']
+#     list_filter = ['price', 'arrival']
+#     prepopulated_field = {'slug': ('title',)}
+#
+#     class Meta:
+#         model = Trip
 
-    class Meta:
-        model = Trip
 
-
-admin.site.register(Trip, TripAdmin)
+# admin.site.register(Trip, TripAdmin)
+admin.site.register(Trip)
+admin.site.register(TripDate)
 admin.site.register(TripImage)
 admin.site.register(Excursion)
