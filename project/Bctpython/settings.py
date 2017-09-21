06a53a19d7f317cf5cdb76c5768f2244cc79a86d
Igor Dubovik <idubovik@milosolutions.com>
2017-09-21 16:17:19 +0300

    Add new settings to settings file

--- project/Bctpython/settings.py
+++ project/Bctpython/settings.py
@@ -184,9 +184,11 @@ CKEDITOR_CONFIGS = {
 }
 
 # Instagram API settings
+# You can define how many photos from instagram will be shown at INSTAGRAM_SHOW_MEDIA_COUNT. Note: only 1-20 photos.
 INSTAGRAM_ACCESS_TOKEN = '2604665802.a200e72.9da5a0eac5e645e89a2245e555921b40'
 INSTAGRAM_CLIENT_ID = 'a200e726a2be40beb58239cf02c08a63'
 INSTAGRAM_CLIENT_SECRET = '64b5b44566b74ec49bdcbc450bc80f4e'
+INSTAGRAM_SHOW_MEDIA_COUNT = 20
 
 # Active Campaign API settings
 ACTIVECAMPAIGN_URL = 'https://testco51607.api-us1.com'
@@ -196,3 +198,9 @@ ACTIVECAMPAIGN_KEY = '92c1ffbfdb174c8688c4c6ac740743d0dd4015d21e9f142d70333a200d
 STRIPE_PUBLIC_KEY = 'pk_test_ptZhV15kJSsxmAFr2dAwihNj'
 STRIPE_SECRET_KEY = 'sk_test_9mNe1308FFjc2YST0JS22Cd6'
 STRIPE_WEBHOOK_SECRET = 'whsec_Vcgs4oARNMP3fp7RJwrhQzHHvulNLBDf'
+
+# User Premium and Points settings
+# Premium membership in dollars
+# Points: for example for membership 50 * 0.2 = 10 points user will earn
+PREMIUM_MEMBERSHIP_PRICE = 50
+POINTS_PER_DOLLAR = 0.2
