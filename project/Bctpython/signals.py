760fd8feacaa5902e7e77be5fe9280edf99132bb
Igor Dubovik <idubovik@milosolutions.com>
2017-09-22 13:10:41 +0300

    Stripe webhook changes

--- project/Bctpython/signals.py
+++ project/Bctpython/signals.py
@@ -1,13 +1,5 @@
 """
 Provides the following signals:
-V1
-- webhook_recurring_payment_failed
-- webhook_invoice_ready
-- webhook_recurring_payment_succeeded
-- webhook_subscription_trial_ending
-- webhook_subscription_final_payment_attempt_failed
-- webhook_subscription_ping_sent
-v2
 - webhook_charge_succeeded
 - webhook_charge_failed
 - webhook_charge_refunded
@@ -41,48 +33,39 @@ v2
 """
 import django.dispatch
 
-WEBHOOK_ARGS = ["customer", "full_json"]
+# stripe webhooks
+WEBHOOK_ARGS = ["full_json"]
 
-webhook_recurring_payment_failed = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
-webhook_invoice_ready = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
-webhook_recurring_payment_succeeded = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
-webhook_subscription_trial_ending = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
-webhook_subscription_final_payment_attempt_failed = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
-webhook_subscription_ping_sent = django.dispatch.Signal(providing_args=[])
-
-# v2 webhooks
-WEBHOOK2_ARGS = ["full_json"]
-
-webhook_charge_succeeded = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_charge_failed = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_charge_refunded = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_charge_disputed = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_customer_created = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_customer_updated = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_customer_deleted = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_customer_subscription_created = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_customer_subscription_updated = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_customer_subscription_deleted = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_customer_subscription_trial_will_end = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_customer_discount_created = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_customer_discount_updated = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_customer_discount_deleted = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_invoice_created = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_invoice_updated = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_invoice_payment_succeeded = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_invoice_payment_failed = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_invoiceitem_created = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_invoiceitem_updated = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_invoiceitem_deleted = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_plan_created = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_plan_updated = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_plan_deleted = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_coupon_created = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_coupon_updated = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_coupon_deleted = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_transfer_created = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_transfer_failed = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
-webhook_ping = django.dispatch.Signal(providing_args=WEBHOOK2_ARGS)
+webhook_charge_succeeded = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_charge_failed = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_charge_refunded = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_charge_disputed = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_customer_created = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_customer_updated = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_customer_deleted = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_customer_subscription_created = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_customer_subscription_updated = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_customer_subscription_deleted = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_customer_subscription_trial_will_end = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_customer_discount_created = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_customer_discount_updated = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_customer_discount_deleted = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_invoice_created = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_invoice_updated = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_invoice_payment_succeeded = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_invoice_payment_failed = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_invoiceitem_created = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_invoiceitem_updated = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_invoiceitem_deleted = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_plan_created = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_plan_updated = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_plan_deleted = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_coupon_created = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_coupon_updated = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_coupon_deleted = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_transfer_created = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_transfer_failed = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
+webhook_ping = django.dispatch.Signal(providing_args=WEBHOOK_ARGS)
 
 WEBHOOK_MAP = {
     'charge_succeeded': webhook_charge_succeeded,
--- project/Bctpython/views.py
+++ project/Bctpython/views.py
@@ -1,13 +1,9 @@
 import stripe
 import json
-
 from django.http import HttpResponse
 from django.conf import settings
-
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-
-from accounts.models import Account
 from .signals import *
 
 
@@ -16,63 +12,9 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
 
 
-def _try_to_get_customer_from_customer_id(stripe_customer_id):
-    try:
-        account = Account.objects.get(stripe_account__customer_id=stripe_customer_id)
-    except Account.DoesNotExists:
-        return None
-    else:
-        return account
-
-
-@csrf_exempt
-def webhooks(request):
-    """
-    Handles all known webhooks from stripe, and calls signals.
-    Plug in as you need.
-    """
-
-    if request.method != "POST":
-        return HttpResponse("Invalid Request.", status=400)
-
-    event_json = json.loads(request.POST["json"])
-
-    if event_json["event"] == "recurring_payment_failed":
-        webhook_recurring_payment_failed.send(sender=None,
-                                              customer=_try_to_get_customer_from_customer_id(event_json["customer"]),
-                                              full_json=event_json)
-
-    elif event_json["event"] == "invoice_ready":
-        webhook_invoice_ready.send(sender=None, customer=_try_to_get_customer_from_customer_id(event_json["customer"]),
-                                   full_json=event_json)
-
-    elif event_json["event"] == "recurring_payment_succeeded":
-        webhook_recurring_payment_succeeded.send(sender=None,
-                                                 customer=_try_to_get_customer_from_customer_id(event_json["customer"]),
-                                                 full_json=event_json)
-
-    elif event_json["event"] == "subscription_trial_ending":
-        webhook_subscription_trial_ending.send(sender=None,
-                                               customer=_try_to_get_customer_from_customer_id(event_json["customer"]),
-                                               full_json=event_json)
-
-    elif event_json["event"] == "subscription_final_payment_attempt_failed":
-        webhook_subscription_final_payment_attempt_failed.send(sender=None,
-                                                               customer=_try_to_get_customer_from_customer_id(
-                                                                   json["customer"]), full_json=event_json)
-
-    elif event_json["event"] == "ping":
-        webhook_subscription_ping_sent.send(sender=None)
-
-    else:
-        return HttpResponse(status=400)
-
-    return HttpResponse(status=200)
-
-
 @require_POST
 @csrf_exempt
-def webhooks_v2(request):
+def webhooks(request):
     """
     Handles all known webhooks from stripe, and calls signals.
     Plug in as you need.
