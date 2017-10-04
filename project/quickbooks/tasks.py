e24ea00be880dbe2ba46f66dfed58dbc15ec3870
Igor Dubovik <idubovik@milosolutions.com>
2017-10-04 19:39:04 +0300

    Add new celery task

--- project/quickbooks/tasks.py
+++ project/quickbooks/tasks.py
@@ -1,7 +1,9 @@
 import requests
+import json
 from django.conf import settings
 from Bctpython.celery import app
-from .models import QuickBooksDiscoveryDocument
+from accounts.quickbooks_api import create_user, create_invoice, invoice_payment
+from .models import QuickBooksDiscoveryDocument, QuickBooksErrorRequest
 
 
 @app.task
@@ -26,3 +28,69 @@ def get_discovery_document():
         return 'Your Discovery Document was created!'
     else:
         return 'Your Discovery Document was updated!'
+
+
+@app.task
+def repeat_quickbooks_errors():
+    customer_errors = QuickBooksErrorRequest.objects.filter(request_type='Customer', status_code=401,
+                                                            successful=False)[:1]
+
+    if customer_errors:
+        get_customer_errors = customer_errors.get()
+        request = json.loads(get_customer_errors.request_body)
+        get_user_username = request['DisplayName']
+        get_user_first_name = request['GivenName']
+        get_user_last_name = request['FamilyName']
+        get_user_email = request['PrimaryEmailAddr']['Address']
+
+        create_quickbooks_customer, status_code = create_user(get_user_username, get_user_first_name,
+                                                              get_user_last_name, get_user_email)
+
+        if not status_code >= 400:
+            get_customer_errors.successful = True
+            get_customer_errors.save()
+        else:
+            return 'Error %s for customer creation' % status_code
+
+        return 'Customer created'
+
+    invoice_errors = QuickBooksErrorRequest.objects.filter(request_type='Invoice', status_code=401,
+                                                           successful=False)[:1]
+
+    if invoice_errors:
+        get_invoice_errors = invoice_errors.get()
+        request = json.loads(get_invoice_errors.request_body)
+        get_customer_id = request['CustomerRef']['value']
+        get_general_price = request['Line'][0]['Amount']
+
+        create_quickbooks_invoice, status_code = create_invoice(get_customer_id, get_general_price)
+
+        if not status_code >= 400:
+            get_invoice_errors.successful = True
+            get_invoice_errors.save()
+        else:
+            return 'Error %s for invoice creation' % status_code
+
+        return 'Invoice Created'
+
+    payment_errors = QuickBooksErrorRequest.objects.filter(request_type='Payment', status_code=401,
+                                                           successful=False)[:1]
+
+    if payment_errors:
+        get_payment_errors = payment_errors.get()
+        request = json.loads(get_payment_errors.request_body)
+        get_customer_id = request['CustomerRef']['value']
+        get_invoice_id = request['Line'][0]['LinkedTxn'][0]['TxnId']
+        get_invoice_amount = request['TotalAmt']
+
+        create_quickbooks_payment, status_code = invoice_payment(get_customer_id, get_invoice_id, get_invoice_amount)
+
+        if not status_code >= 400:
+            get_payment_errors.successful = True
+            get_payment_errors.save()
+        else:
+            return 'Error %s for payment creation' % status_code
+
+        return 'Payment Created'
+
+    return 'There are no QuickBooks errors!'
