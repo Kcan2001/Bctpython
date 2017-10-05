09cd78b8f1f8c337574847a16c5ec96f64122e33
Igor Dubovik <idubovik@milosolutions.com>
2017-10-05 16:46:42 +0300

    Add Sentry Raven to Celery file

--- project/Bctpython/celery.py
+++ project/Bctpython/celery.py
@@ -1,6 +1,21 @@
 from __future__ import absolute_import, unicode_literals
-import os
 from celery import Celery
+import raven
+from raven.contrib.celery import register_signal, register_logger_signal
+import os
+
+
+class Celery(Celery):
+    def on_configure(self):
+        client = raven.Client(
+            'http://da385935ce644993bc71402cf42a45aa:f12c65a8409e40eb8158350f274f193a@sentry.milosolutions.com/64')
+
+        # register a custom filter to filter out duplicate logs
+        register_logger_signal(client)
+
+        # hook into the Celery error handler
+        register_signal(client)
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bctpython.settings')
